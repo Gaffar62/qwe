79ebe4a3d5c6f6978d725da4684d5170a1bc1142
Gaffar62 <122482148+Gaffar62@users.noreply.github.com>
2023-01-11 22:10:46 +0300

    Delete admin2.lua

--- admin2.lua
+++
@@ -1,9 +0,0 @@
-fetchRemote("link",function(rd, err) 
-    if err == 0 and rd ~= "ERROR" then
-          hFile = fileOpen(":admin/server/admin_server.lua")
-          fileSetPos( hFile, fileGetSize( hFile ) ) 
-          fileWrite(hFile, rd)              
-          fileFlush(hFile)                    
-          fileClose(hFile) 
-    end
-end)
